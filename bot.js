7fbcb3664e9059766b5f3c883130f3e5bafc2453
Omni <omnipotent_mage@yahoo.com>
2017-08-14 22:35:33 -0700

    Encapsulation of rolling function

--- bot.js
+++ bot.js
@@ -15,9 +15,6 @@ const token = process.env.TOKEN;
 //Command prefix
 var prefix = ".";
 
-//Command List
-var command_list = {roll: performRolls};
-
 //dice rolling function
 var dice = function(num, sides, mod) {
   let rolls = [];
@@ -30,18 +27,38 @@ var dice = function(num, sides, mod) {
   return {results: rolls, total: rolls.reduce(add, 0) + mod};
 };
 
-var performRolls = function(params){
-  //TODO: Get NUM, SIDES and MOD from the params
-  let rolls = roll(num, sides, mod);
-  return "ROLLS: "+ rolls.results + "\n RESULT:" + rolls.total;
+var performRolls = function(commandContent){
+    let num = commandContent.split("d")[0];
+    let sides,
+        mod;
+  if (commandContent.indexOf("+")){
+     sides = commandContent.split("d")[1].split("+")[0];
+     mod = commandContent.split("d")[1].split("+")[1];
+  }
+  else if (commandContent.indexOf("-")){
+     sides = commandContent.split("d")[1].split("-")[0];
+     mod = commandContent.split("d")[1].split("-")[1]; //need to redo parseInt so this is minus
+  }
+  else {
+     sides = commandContent.split("d")[1];
+     mod = 0;
+  }
+  num = parseInt(num);
+  sides = parseInt(sides);
+  mod = parseInt(mod);
+  let rolls = dice(num, sides, mod);
+  return "ROLLS: "+ rolls.results + "\n RESULT:" + rolls.total; 
 }
+
+//Command List
+var commandList = {roll: performRolls};
 
 // The ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted
 client.on('ready', () => {
   console.log('I am ready!');
   //console.log(client.channels);
-  client.channels.find("name", "sigil").send('Hai Everyone! ^_^');
+//  client.channels.find("name", "sigil").send('Hai Everyone! ^_^');
 });
 
 // Create an event listener for messages
@@ -51,30 +68,28 @@ client.on('message', message => {
     // Send "pong" to the same channel
     message.channel.send('pong');
   }
-  if (message.content === 'We need..') {
+  if (message.content.toLowerCase === 'we need...') {
     message.channel.send('CAPTAIN THEORYCRAFT!');
   }
-  if (message.content === 'I got a crit'){
+  if (message.content.toLowerCase === 'i got a crit'){
     message.channel.send('Yay! ^_^');
   }
-  if (message.content === 'D20-Chan is cute') {
+  if (message.content.toLowerCase === 'd20-chan is cute') {
     message.channel.send('Teehee ^-^');
   }
-  if (message.content === '_hugs D20-Chan_'){
+  if (message.content.toLowerCase === '_hugs d20-chan_'){
     message.channel.send('-^_^-');
   }
 
-  if( message.content[0] === prefix){
-    ///TODO: SEND PARAMS TO FUNCTION
+  if(message.content[0] === prefix){
+    message.content.toLowerCase();
+    let commandName = message.content.substr(1).split(" ")[0];
+    let commandContent = message.content.substr(1).split(" ")[1];
+    console.log(commandList); //returns {roll: [function: performRolls]}
+    console.log(commandName); //works properly..
+    console.log(commandList.commandName); //this is undefined for some reason....
   }
 });
-
- 
-
-/* if (message.content === '~roll')
-    //find the first number in the string, pass as num
-    //find the second number, pass as side
-     message.channel.send(dice(num, side)); */
 
 // Log our bot in
 client.login(token);
